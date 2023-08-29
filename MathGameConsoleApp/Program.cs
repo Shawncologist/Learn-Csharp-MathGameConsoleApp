9fa47b7ef924cbd29bc59766830cb4f901e631f2
Shawn <34050346+Shawncologist@users.noreply.github.com>
2023-08-29 09:08:21 -0700

    Add project files.

---
+++ MathGameConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace MathGameConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name, please?");
+            string name = Console.ReadLine();
+            var date = DateTime.UtcNow;
+
+            Console.WriteLine("\n - + - + - + - + - + - + - ");
+            Console.WriteLine($"Hello {name}, happy {date.DayOfWeek}! Would you like to do some inconvenient console math?\n");
+            Console.WriteLine(@$"Please choose your calculation type below:
+A - Addition
+S - Subtraction
+M - Multiplication
+D - Division
+Q - Quit this program");
+            Console.WriteLine(" - + - + - + - + - + - + - ");
+
+
+
+        }
+    }
+}
