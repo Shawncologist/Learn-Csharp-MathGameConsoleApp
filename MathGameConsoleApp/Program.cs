3bee7dcd261112052467a075b0c5f199953a9598
Shawn <34050346+Shawncologist@users.noreply.github.com>
2023-08-30 07:46:04 -0700

    refactored to use switch rather than if else.

--- MathGameConsoleApp/Program.cs
+++ MathGameConsoleApp/Program.cs
@@ -8,43 +8,8 @@
             string name = Console.ReadLine();
             var date = DateTime.UtcNow;
 
-            Console.WriteLine("\n - + - + - + - + - + - + - ");
-            Console.WriteLine($"Hello {name}, happy {date.DayOfWeek}! Would you like to do some inconvenient console math?\n");
-            Console.WriteLine(@$"Please choose your calculation type below:
-A - Addition
-S - Subtraction
-M - Multiplication
-D - Division
-Q - Quit this program");
-            Console.WriteLine(" - + - + - + - + - + - + - ");
+            Menu(name, date);
 
-            var userChoice = Console.ReadLine();
-
-            if (userChoice.Trim().ToLower() == "a")
-            {
-                AdditionGame("You've selected Addition");
-            }
-            else if (userChoice.Trim().ToLower() == "s")
-            {
-                SubtractionGame("You've selected Subtraction");
-            }
-            else if (userChoice.Trim().ToLower() == "m") 
-            {
-                MultiplicationGame("You've selected Multiplication");
-            }
-            else if (userChoice.Trim().ToLower() == "d")
-            {
-                DivisionGame("You've selected Division");
-            }
-            else if (userChoice.Trim().ToLower() == "q")
-            {
-                Console.WriteLine("Thank you for your time, goodbye");
-                Environment.Exit(1);
-            }
-            else
-            {
-                Console.WriteLine("Invalid selection");
-            }
             void AdditionGame(string message)
             {
                 Console.WriteLine(message);
@@ -60,6 +25,44 @@ Q - Quit this program");
             void DivisionGame(string message)
             {
                 Console.WriteLine(message);
+            }
+
+            void Menu(string name, DateTime date)
+            {
+                Console.WriteLine("\n - + - + - + - + - + - + - ");
+                Console.WriteLine($"Hello {name}, happy {date.DayOfWeek}! Would you like to do some inconvenient console math?\n");
+                Console.WriteLine(@$"Please choose your calculation type below:
+A - Addition
+S - Subtraction
+M - Multiplication
+D - Division
+Q - Quit this program");
+                Console.WriteLine(" - + - + - + - + - + - + - ");
+
+                var userChoice = Console.ReadLine();
+
+                switch (userChoice.Trim().ToLower())
+                {
+                    case "a":
+                        AdditionGame("You've selected Addition");
+                        break;
+                    case "s":
+                        SubtractionGame("You've selected Subtraction");
+                        break;
+                    case "m":
+                        MultiplicationGame("You've selected Multiplication");
+                        break;
+                    case "d":
+                        DivisionGame("You've selected Division");
+                        break;
+                    case "q":
+                        Console.WriteLine("Thank you for your time, goodbye");
+                        Environment.Exit(1);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid selection");
+                        break;
+                }
             }
         }
     }
